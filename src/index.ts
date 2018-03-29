fd51faed70499e852278ac54cd32e0aa92e95786
Git Automerge <jcguarinpenaranda@gmail.com>
2018-03-29 13:33:56 -0500

    created api

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
-export class MyModule {
-  
+/**
+ * Paginates
+ * @param query 
+ */
+export async function paginateQuery(
+  query: Parse.Query,
+): Promise<{total: number, limit: number, skip: number, results: Parse.Object[]}> {
+  const total = await query.count();
+  const results = await query.find();
+  const json = query.toJSON();
+
+  return {
+    total,
+    results,
+    limit: json.limit,
+    skip: json.skip,
+  };
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,29 @@
-import { MyModule } from '../src/index';
+import * as Parse from 'parse/node';
+import { paginateQuery } from '../src';
 
-test('test', () => {
-  expect(true).toBe(true);
+test('should return the right pagination parameters', async () => {
+  const obj = new Parse.Object('TestObj');
+  obj.id = '123';
+
+  const query = new Parse.Query('TestClass');
+  query.limit(20);
+  query.skip(50);
+
+  // Spy the query count function to return some value
+  // and avoid network contact
+  jest.spyOn(query, 'count').mockImplementationOnce(async () => 2000);
+
+  // Spy the query find function to return some value
+  // and avoid network contact
+  jest.spyOn(query, 'find').mockImplementationOnce(async () => [obj]);
+
+  // Call the function
+  const result = await paginateQuery(query);
+  
+  expect(result).toEqual({
+    total: 2000,
+    results: [obj],
+    limit: 20,
+    skip: 50,
+  });
 });
